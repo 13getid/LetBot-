bdfbc6b4f962e2ef69f8538c1c338cf5d5cde554
IAN <odhiamian74@gmail.com>
2025-12-05 12:10:32 +0300

    Initial Commit

--- app/src/main/java/com/example/letbot/ChatPage.kt
+++ app/src/main/java/com/example/letbot/ChatPage.kt
@@ -1,10 +1,82 @@
 package com.example.letbot
 
+import android.app.Application
+import android.appwidget.AppWidgetHost
+import android.os.Message
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ChatPage(modifier: Modifier = Modifier){
-    Text(text = "Chat Page")
+    Column(
+        modifier = Modifier
+    ) {
+        AppHeader()
+        MessageInput()
+    }
+}
+
+@Composable
+fun MessageInput(){
+    var  message by remember {
+        mutableStateOf("")
+    }
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        OutlinedTextField(
+            modifier = Modifier.weight(1f),
+            value = message, onValueChange ={
+            message = it
+        } )
+        IconButton(onClick = { }) {
+            Icon(
+                imageVector = Icons.Default.Send,
+                contentDescription = "Send")
+        }
+
+    }
+}
+@Composable
+fun AppHeader(){
+    Box(
+        modifier = Modifier
+            .padding(vertical = 64.dp)
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primary)
+    ){
+        Text(
+            modifier = Modifier.padding(32.dp),
+            text = "Let Bot",
+            color = Color.White,
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold
+            )
+    }
 }
