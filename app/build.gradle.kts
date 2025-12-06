590ac73c372d89f813378dcbd52fe1b0f6235052
IAN <odhiamian74@gmail.com>
2025-12-07 01:28:42 +0300

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -59,5 +60,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     // Import the BoM for the firebase platform
     implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
-    implementation("com.google.firebase:firebase-ai")
+    implementation("com.google.firebase:firebase-ai:16.0.0")
 }
--- app/src/main/java/com/example/letbot/ChatViewModel.kt
+++ app/src/main/java/com/example/letbot/ChatViewModel.kt
@@ -6,15 +6,30 @@ import androidx.lifecycle.viewModelScope
 import com.google.firebase.Firebase
 import com.google.firebase.ai.ai
 import com.google.firebase.ai.type.GenerativeBackend
-import com.google.firebase.ai.type.content
 import kotlinx.coroutines.launch
+import java.lang.Exception
+import kotlin.math.log
 
 class ChatViewModel : ViewModel () {
 
     // initializing the generative model
     val model = Firebase.ai(backend = GenerativeBackend.googleAI())
         .generativeModel("gemini-2.5-flash")
-    fun sendMessage(question : String) {
 
+    // start a persistent chat session
+    private  val  chatSession = model.startChat()
+    //define a tag for your log messages
+    private val tag = "GeminiChatbot"
+    fun sendMessage(question : String) {
+        viewModelScope.launch {
+            try {
+                Log.d(tag,"Sending message: $question")// log that message is being sent
+                val response = chatSession.sendMessage(question)
+                // log the response text specifically to logcat
+                Log.d(tag,"Received response:${response.text}")
+            }catch (e: Exception){
+                Log.e(tag,"Error getting response",e)// Log any errors
+            }
+        }
     }
     }
