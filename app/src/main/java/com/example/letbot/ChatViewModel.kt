26c72429d24c9c448d2500514f7c7b29d181682e
IAN <odhiamian74@gmail.com>
2026-02-10 10:53:02 +0300

    Initial commit

--- app/src/main/java/com/example/letbot/ChatViewModel.kt
+++ app/src/main/java/com/example/letbot/ChatViewModel.kt
@@ -3,11 +3,16 @@ package com.example.letbot
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.Firebase
+import com.google.firebase.ai.ai
+import com.google.firebase.ai.type.GenerativeBackend
 import kotlinx.coroutines.launch
 
 class ChatViewModel : ViewModel () {
 
     // initializing the generative model
+    val model = Firebase.ai(backend = GenerativeBackend.googleAI())
+        .generativeModel("gemini-2.5-flash")
 
     // start a persistent chat session
     private  val  chatSession = model.startChat()
